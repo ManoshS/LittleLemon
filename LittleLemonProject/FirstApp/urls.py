17f2dc353b14091245dcb365e538ad4e9bf34303
Manosh <smanosh73@gmail.com>
2023-12-24 08:25:44 +0530

    Adding django rest framework to ourproject

--- LittleLemonProject/FirstApp/urls.py
+++ LittleLemonProject/FirstApp/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('home/', views.home,name='home'),
+    path('booking/',views.BookingView.as_view(),name='Booking')
 ]
--- LittleLemonProject/FirstApp/views.py
+++ LittleLemonProject/FirstApp/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import BookingSerializer
 
+from .models import Booking
 # Create your views here.
 def home(request):
     return render(request,'index.html',{'current_year':2024})
+
+class BookingView(APIView):
+    def get(request,pk):
+        item=Booking.objects.all()
+        serializer=BookingSerializer(item,many=True)
+        return Response(serializer.data)
+    
+    def post(request):
+        serializer=BookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response({'status':"Success",'data':serializer.data})
